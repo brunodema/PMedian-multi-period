1ada3e30a13da1decf226856d5c010fb40681ce2
brunodema <64618343+brunodema@users.noreply.github.com>
2020-10-08 20:41:58 -0300

    Add files via upload
    included a sample '.bat' file for tests

---
+++ test.bat
@@ -0,0 +1,80 @@
+# custo fixo
+%1 -val 3 3 100 1000 3 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol1 -o %3
+%1 -val 3 3 100 1000 3 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol2 -o %3
+%1 -val 3 3 100 1000 3 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol3 -o %3
+
+%1 -val 3 3 100 5000 3 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol4 -o %3
+%1 -val 3 3 100 5000 3 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol5 -o %3
+%1 -val 3 3 100 5000 3 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol6 -o %3
+
+%1 -val 3 3 100 10000 3 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol7 -o %3
+%1 -val 3 3 100 10000 3 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol8 -o %3
+%1 -val 3 3 100 10000 3 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol9 -o %3
+
+# numero de depositos
+%1 -val 3 10 100 5000 10 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol10 -o %3
+%1 -val 3 10 100 5000 10 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol11 -o %3
+%1 -val 3 10 100 5000 10 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol12 -o %3
+
+%1 -val 3 30 100 5000 30 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol13 -o %3
+%1 -val 3 30 100 5000 30 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol14 -o %3
+%1 -val 3 30 100 5000 30 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol15 -o %3
+
+%1 -val 3 100 100 5000 100 100 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol16 -o %3
+%1 -val 3 100 100 5000 100 100 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol17 -o %3
+%1 -val 3 100 100 5000 100 100 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol18 -o %3
+
+# numero de clientes
+%1 -val 3 30 100 5000 30 500 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol19 -o %3
+%1 -val 3 30 100 5000 30 500 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol20 -o %3
+%1 -val 3 30 100 5000 30 500 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol21 -o %3
+
+%1 -val 3 30 100 5000 30 1000 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol22 -o %3
+%1 -val 3 30 100 5000 30 1000 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol23 -o %3
+%1 -val 3 30 100 5000 30 1000 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol24 -o %3
+
+%1 -val 3 30 100 5000 30 3000 100 1 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol25 -o %3
+%1 -val 3 30 100 5000 30 3000 100 1 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol26 -o %3
+%1 -val 3 30 100 5000 30 3000 100 1 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol27 -o %3
+
+## mesma coisa, com tres categorias diferentes agora
+# custo fixo
+%1 -val 3 3 100 1000 3 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol28 -o %3
+%1 -val 3 3 100 1000 3 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol29 -o %3
+%1 -val 3 3 100 1000 3 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol30 -o %3
+
+%1 -val 3 3 100 5000 3 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol31 -o %3
+%1 -val 3 3 100 5000 3 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol32 -o %3
+%1 -val 3 3 100 5000 3 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol33 -o %3
+
+%1 -val 3 3 100 10000 3 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\so34 -o %3
+%1 -val 3 3 100 10000 3 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\so35 -o %3
+%1 -val 3 3 100 10000 3 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\so36 -o %3
+
+# numero de depositos
+%1 -val 3 10 100 5000 10 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol37 -o %3
+%1 -val 3 10 100 5000 10 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol38 -o %3
+%1 -val 3 10 100 5000 10 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol39 -o %3
+
+%1 -val 3 30 100 5000 30 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol40 -o %3
+%1 -val 3 30 100 5000 30 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol41 -o %3
+%1 -val 3 30 100 5000 30 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol42 -o %3
+
+%1 -val 3 100 100 5000 100 100 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol43 -o %3
+%1 -val 3 100 100 5000 100 100 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol44 -o %3
+%1 -val 3 100 100 5000 100 100 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol45 -o %3
+
+# numero de clientes
+%1 -val 3 30 100 5000 30 500 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol46 -o %3
+%1 -val 3 30 100 5000 30 500 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol47 -o %3
+%1 -val 3 30 100 5000 30 500 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol48 -o %3
+
+%1 -val 3 30 100 5000 30 1000 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol49 -o %3
+%1 -val 3 30 100 5000 30 1000 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol50 -o %3
+%1 -val 3 30 100 5000 30 1000 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol51 -o %3
+
+%1 -val 3 30 100 5000 30 3000 100 3 1000 1000 -rng 1 -runtime 3600 -draw %2\\sol52 -o %3
+%1 -val 3 30 100 5000 30 3000 100 3 1000 1000 -rng 2 -runtime 3600 -draw %2\\sol53 -o %3
+%1 -val 3 30 100 5000 30 3000 100 3 1000 1000 -rng 3 -runtime 3600 -draw %2\\sol54 -o %3
+
+pause
